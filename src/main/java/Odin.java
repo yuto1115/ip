cac5463ed86dfe317946542f15d960f9e2f5e2fb
yuto1115 <yutown1115@gmail.com>
2025-01-24 17:15:01 +0800

    add mark/unmark feature

--- src/main/java/Odin.java
+++ src/main/java/Odin.java
@@ -2,17 +2,21 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Odin {
-    static final String SEPARATOR = "____________________________________________________________";
-    static final Scanner scanner = new Scanner(System.in);
+    private static final String SEPARATOR = "____________________________________________________________";
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         System.out.println(SEPARATOR);
         speak("I am Odin, god of wisdom.", "What knowledge do you seek?");
 
-        ArrayList<String> tasks = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
 
         while (true) {
             String input = listen();
+            ArrayList<String> tokens = new ArrayList<>();
+            for (String token : input.split(" ")) {
+                if (!token.isEmpty()) tokens.add(token);
+            }
             if (input.equals("bye")) {
                 break;
             } else if (input.equals("list")) {
@@ -22,8 +26,23 @@ public class Odin {
                     messages.add(String.format("%d. %s", i + 1, tasks.get(i)));
                 }
                 speak(messages);
+            } else if (tokens.size() >= 2 && (tokens.get(0).equals("mark") || tokens.get(0).equals("unmark"))) {
+                if (tokens.size() != 2 || !isInteger(tokens.get(1))) {
+                    speak("Your format is flawed. The correct format is '(mark|unmark) [task index]'.");
+                } else {
+                    int index = Integer.parseInt(tokens.get(1));
+                    if (index <= 0 || index > tasks.size()) {
+                        speak(String.format("The task index is incorrect. There are tasks numbered 1 through %d.", tasks.size()));
+                    } else if (tokens.get(0).equals("mark")) {
+                        tasks.get(index - 1).markAsDone();
+                        speak(String.format("Task %d has been marked as completed.", index), tasks.get(index - 1).toString());
+                    } else {
+                        tasks.get(index - 1).markAsNotDone();
+                        speak(String.format("Task %d has been marked as NOT completed.", index), tasks.get(index - 1).toString());
+                    }
+                }
             } else {
-                tasks.add(input);
+                tasks.add(new Task(input));
                 speak(String.format("The task \"%s\" has been noted.", input));
             }
         }
@@ -58,5 +77,9 @@ public class Odin {
         String input = scanner.nextLine();
         System.out.println(SEPARATOR);
         return input;
+    }
+
+    private static boolean isInteger(String str) {
+        return str.matches("\\d+");
     }
 }
