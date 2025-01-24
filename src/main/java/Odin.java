50ca685e4f7f7ce75b41c7cf9d8712d4706c5ff1
yuto1115 <yutown1115@gmail.com>
2025-01-24 16:40:56 +0800

    add task add/list feature

--- src/main/java/Odin.java
+++ src/main/java/Odin.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Odin {
@@ -7,17 +8,47 @@ public class Odin {
     public static void main(String[] args) {
         System.out.println(SEPARATOR);
         speak("I am Odin, god of wisdom.", "What knowledge do you seek?");
+
+        ArrayList<String> tasks = new ArrayList<>();
+
         while (true) {
             String input = listen();
-            if (input.equals("bye")) break;
-            speak(input);
+            if (input.equals("bye")) {
+                break;
+            } else if (input.equals("list")) {
+                ArrayList<String> messages = new ArrayList<>();
+                messages.add("These are the tasks upon the list:");
+                for (int i = 0; i < tasks.size(); i++) {
+                    messages.add(String.format("%d. %s", i + 1, tasks.get(i)));
+                }
+                speak(messages);
+            } else {
+                tasks.add(input);
+                speak(String.format("The task \"%s\" has been noted.", input));
+            }
         }
+
         speak("Bye. We shall meet again.");
     }
 
     private static void speak(String... messages) {
-        for (String message : messages) {
-            System.out.println("Odin: " + message);
+        for (int i = 0; i < messages.length; i++) {
+            if (i == 0) {
+                System.out.println("Odin: " + messages[i]);
+            } else {
+                System.out.println("      " + messages[i]);
+            }
+        }
+        System.out.println(SEPARATOR);
+    }
+
+    private static void speak(ArrayList<String> messages) {
+        for (int i = 0; i < messages.size(); i++) {
+            if (i == 0) {
+                System.out.println("Odin: " + messages.get(i));
+            } else {
+                System.out.println("      " + messages.get(i));
+            }
         }
         System.out.println(SEPARATOR);
     }
