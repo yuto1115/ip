828aef7952c6256c6e3ce85c9bf9af1304b72c65
yuto1115 <yutown1115@gmail.com>
2025-01-24 16:23:22 +0800

    Echo

--- src/main/java/Odin.java
+++ src/main/java/Odin.java
@@ -1,10 +1,31 @@
+import java.util.Scanner;
+
 public class Odin {
-    static final String NAME = "Odin";
-    static final String SEPARATOR = "____________________________________________________________\n";
+    static final String SEPARATOR = "____________________________________________________________";
+    static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        String greet = String.format("Hello! I'm %s\nWhat can I do for you?\n", NAME);
-        String exit = "Bye. Hope to see you again soon!\n";
-        System.out.println(SEPARATOR + greet + SEPARATOR + exit + SEPARATOR);
+        System.out.println(SEPARATOR);
+        speak("I am Odin, god of wisdom.", "What knowledge do you seek?");
+        while (true) {
+            String input = listen();
+            if (input.equals("bye")) break;
+            speak(input);
+        }
+        speak("Bye. We shall meet again.");
+    }
+
+    private static void speak(String... messages) {
+        for (String message : messages) {
+            System.out.println("Odin: " + message);
+        }
+        System.out.println(SEPARATOR);
+    }
+
+    private static String listen() {
+        System.out.print("You: ");
+        String input = scanner.nextLine();
+        System.out.println(SEPARATOR);
+        return input;
     }
 }
