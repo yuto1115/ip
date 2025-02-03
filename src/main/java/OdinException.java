f56120eca86619510c284c184529d9c982879450
yuto1115 <yutown1115@gmail.com>
2025-02-04 01:54:27 +0800

    Change exception handling scheme

--- src/main/java/OdinException.java
+++ src/main/java/OdinException.java
@@ -1,5 +1,13 @@
+import java.util.ArrayList;
+
 public class OdinException extends Exception {
     OdinException(String message) {
         super(message);
+    }
+
+    public ArrayList<String> getMessageList() {
+        ArrayList<String> messages = new ArrayList<>();
+        messages.add(this.getMessage());
+        return messages;
     }
 };
